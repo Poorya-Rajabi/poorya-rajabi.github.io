1c26fe66d63532d7a99c02944614fb0492fb7f10
poorya-rajabi <poorya.rajabi.p@gmail.com>
2023-07-15 00:36:59 +0330

    feat: tars update model and create animation

--- mixins/loaders.js
+++ mixins/loaders.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import gsap from "gsap";
+import gsap from "gsap";
 
 export default {
   data() {
@@ -77,18 +77,36 @@ export default {
             return mesh.name === 'Tars004'
           })
 
-          // this.gui.add(x1.rotation, 'x').min(-20).max(20).step(0.01)
-          // this.gui.add(x1.rotation, 'y').min(-20).max(20).step(0.01)
-          // this.gui.add(x1.rotation, 'z').min(-20).max(20).step(0.01)
+          // this.gui.add(x1.rotation, 'z').min(-20).max(20).step(0.01).name('x1')
+          // this.gui.add(x2.rotation, 'z').min(-20).max(20).step(0.01).name('x2')
+          // this.gui.add(x3.rotation, 'z').min(-20).max(20).step(0.01).name('x3')
+          // this.gui.add(x4.rotation, 'z').min(-20).max(20).step(0.01).name('x4')
 
           const tars = new THREE.Group()
           tars.add(x1, x2, x3, x4)
           tars.scale.set(0.5, 0.5, 0.5)
           tars.position.set(-1.93, -0.78, 9.15)
 
-          this.gui.add(tars.position, 'x').min(-20).max(20).step(0.01)
-          this.gui.add(tars.position, 'y').min(-20).max(20).step(0.01)
-          this.gui.add(tars.position, 'z').min(-20).max(20).step(0.01)
+          gsap.to(
+            x1.rotation,
+            { z: -Math.PI * 0.5, duration: 0.8, delay: 3 })
+          gsap.to(
+            x3.rotation,
+            { z: -Math.PI * 0.2, duration: 0.8, delay: 3 })
+          gsap.to(
+            x4.rotation,
+            { z: -Math.PI * 0.8, duration: 0.8, delay: 3 })
+
+
+          gsap.to(
+            tars.position,
+            { x: 5, duration: 3, delay: 3.8 })
+          gsap.to(
+            tars.rotation,
+            { z: -Math.PI * 4, duration: 3, delay: 3.8 })
+          // this.gui.add(tars.position, 'x').min(-20).max(20).step(0.01)
+          // this.gui.add(tars.position, 'y').min(-20).max(20).step(0.01)
+          // this.gui.add(tars.position, 'z').min(-20).max(20).step(0.01)
 
           this.scene.add(tars)
           this.tars = tars
